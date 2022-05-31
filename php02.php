f017bfb1574b541c7c4c7855c30eb5c364455f74
Hasibul10884 <hasibulhasan193@gmail.com>
2022-05-31 10:46:06 +0600

    gg

---
+++ php02.php
@@ -0,0 +1,16 @@
+<?php 
+//logical operator
+$Hasib = "400";
+$farhana = "400";
+if ($Hasib == $farhana){
+    echo "Hasib has same number of money as farhana";
+}
+elseif ($Hasib > $farhana){
+    echo "Hasib has more money than farhana";
+}
+else if ($Hasib < $farhana){
+    echo "Hasib has less money than farhana";
+}
+else if ($Hasib >= $farhana){
+    echo "Hasib has same or more money as farhana";
+}
