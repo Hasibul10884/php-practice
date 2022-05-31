8b64da41affa5aae7530e93bb3c88894bd914805
Hasibul10884 <hasibulhasan193@gmail.com>
2022-05-31 23:19:53 +0600

    dd

---
+++ php05.php
@@ -0,0 +1,16 @@
+<?php 
+class Human{
+   function sayHi(){
+       echo "salam";
+   } 
+}
+class Cat{
+    function sayHi(){
+        echo "meow";
+    }
+}
+class Dog{
+    function sayHi(){
+        echo "woof";
+    }
+}
