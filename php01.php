14f62c9a44f87abf8a623ba1737855492386c68c
Hasibul10884 <hasibulhasan193@gmail.com>
2022-05-31 10:30:58 +0600

    commit

---
+++ php01.php
@@ -0,0 +1,17 @@
+<?php
+$n = 7;
+if ($n % 2 == 0){
+    echo "$n is an even number";
+}
+else{
+    echo "$n is a odd number";
+}
+if ($n > 10){
+    echo "$n is greater than 10";
+}
+elseif ($n < 10){
+    echo "$n is less than 10";
+}
+else{
+    echo "$n is equal to 10";
+}
