bf8d2144e609df41cb2398a655f20c49046fdf3b
Hasibul10884 <hasibulhasan193@gmail.com>
2022-06-02 22:13:00 +0600

    cc

--- php05.php
+++ php05.php
@@ -1,4 +1,5 @@
 <?php 
+//class method
 class Human{
     public $name;
     public $age;
@@ -21,13 +22,13 @@ class Dog{
     }
 }
 $h1 = new Human();
-$h1->name = "John";
-$h1->age = 20;
+$h1->name = "John";//set
+$h1->age = 20;//set
 $c1 = new Cat();
 $d1 = new Dog();
 
 $h1->sayHi();
 echo $h1-> name;
-echo $h1->age;
-$c1->sayHi();
+echo $h1->age;//get
+$c1->sayHi();//get
 $d1->sayHi();
---
+++ php06.php
@@ -0,0 +1,35 @@
+<?php 
+//integer
+$a = 10;
+var_dump($a);
+?>
+
+<?php 
+//float
+$a = 10.5;
+var_dump($a);
+?>
+
+<?php 
+//string
+$a = "Hello";
+$b = 'Hello!';
+echo $a;
+echo "<br>";
+echo $b;
+?>
+
+<?php 
+//boolean
+$a = true;
+$b = false;
+var_dump($a);
+var_dump($b);
+?>
+
+<?php 
+//null
+$a = "hello world";
+$a = null;
+var_dump($a);
+?>
